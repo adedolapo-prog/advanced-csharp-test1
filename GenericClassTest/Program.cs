e26d557895622d94eb3ec547c54d6cc662a6720c
adedolapo-prog <shayne.doly94@gmail.com>
2024-04-23 17:49:11 -0700

    Add project files.

---
+++ GenericClassTest/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericClassTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class valueConstraintClass<T> where T: struct
+    {
+        private List<T> _valueList;
+
+        public void add(T value) {
+            _valueList.Add(value);
+        }
+
+        public T getSingleValue(int index)
+        {
+            return _valueList[index];
+        }
+
+        public IEnumerable<T> getValuesInDescOrder() {
+            return _valueList.OrderByDescending(x => x);
+        }
+    }
+}
